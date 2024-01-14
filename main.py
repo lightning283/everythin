4434149e22d9cf344edbde0cd973a4ac1381e6d2
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-01-14 08:59:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import eel
 import os
 from pytube import YouTube
+
 eel.init("web")
+
+
 @eel.expose
-def download_video(link):
+def download_video(link: str) -> str:
     print("Download Started")
     eel.set_loading_cursor()
     yt = YouTube(link)
     audio_stream = yt.streams.get_lowest_resolution()
-    if os.path.isdir("web/downloads/") == True:
-        pass
-    else:
+    if os.path.isdir("web/downloads/") != True:
         os.mkdir("web/downloads/")
     audio_stream.download("web/downloads/")
-    url = f'{yt.title}.mp4'
+    url = f"{yt.title}.mp4"
     eel.reset_cursor()
     return url
+
+
 eel.start("index.html")
